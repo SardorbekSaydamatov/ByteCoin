b8214bf2385853bc33e61a2b38d7cd00a1e2c959
Sardorbek Saydamatov <sardorbeksaydamatov@SardorbeksAir2.home>
2022-12-12 23:25:30 +0500

    first commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,15 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Sardorbek Saydamatov on 01/12/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinData: Decodable{
+    
+    let rate: Double
+}
